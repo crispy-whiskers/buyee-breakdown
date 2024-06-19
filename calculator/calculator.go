a389f02c5794273e20b9486329b68f207ca21057
crispy-whiskers <nguyen.k.allen@gmail.com>
2024-06-19 01:10:57 -0400

    rip og calc code

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -4,11 +4,14 @@ type Person struct {
 	string             //name
 	proportion float32 //percentage
 	iou        float32
+	ship_b4    float32
+	ship_total float32
+	item_total float32
 }
 
 type Item struct {
-	link     []string
-	person   []Person
+	link     string
+	person   Person
 	yen      float32
 	shipping float32
 }
@@ -17,4 +20,23 @@ type Calculator struct {
 	people         []Person
 	items          []Item
 	total_shipping float32
+	batched        float32
+}
+
+func (c *Calculator) Sum_shipping() {
+
+	for _, e := range c.items {
+		c.total_shipping += e.shipping
+		e.person.ship_b4 += e.shipping
+		e.person.item_total += e.yen
+	}
+}
+
+func (c *Calculator) Break_shipping_down() {
+	c.Sum_shipping()
+
+	if c.total_shipping == 0 || c.batched == 0 {
+		return
+	}
+
 }
